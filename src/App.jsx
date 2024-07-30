8f8e3f1cf734aa94f1a33374ca6197ee608ef141
abc <you@example.com>
2024-07-30 16:38:03 +0330

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const 
+const [progress,setProgress]=useState(0);
   function handlePrv(){
-    console.log("first")
+    if(progress>0)
+    setProgress(progress-1)
+  } function handleNext(){
+    if(progress<4)
+    setProgress(progress+1)
   }
     return (
         <div>
           <div className="container">
             <div className="container-progress">
-                <div className="circle">1</div>
-                <div className="line"></div>
-                <div className="circle">2</div>
-                <div className="line"></div>
-                <div className="circle">3</div>
+                <div className= {progress>=0? 'circle fill': 'cricle'}>1</div>
+                <div className={progress>=1?"line fill2":'line'}></div>
+                <div className={progress>=2? 'circle fill': 'circle'}>2</div>
+                <div className={progress>=3?"line fill2":'line'}></div>
+                <div className={progress>=4? 'circle fill': 'circle'}>3</div>
             </div>
             <div className="buttons">
                 <button className="btn" onClick={handlePrv}>prv</button>
-                <button className="btn">next</button>
+                <button className="btn" onClick={handleNext}>next</button>
             </div>
             </div>
         </div>
